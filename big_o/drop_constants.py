87959915aa75e4f87989391bec84212e67a4fa3e
EC2 Default User <ec2-user@ip-172-31-86-220.ec2.internal>
2023-03-14 19:07:42 +0000

    big o

---
+++ big_o/drop_constants.py
@@ -0,0 +1,8 @@
+def print_items(n):
+    for item in range(n):
+        print(item)
+        
+    for j in range(n):
+        print(j)
+
+print_items(10)
---
+++ big_o/o_n.py
@@ -0,0 +1,5 @@
+def print_items(n):
+    for item in range(n):
+        print(item)
+
+print_items(10)
